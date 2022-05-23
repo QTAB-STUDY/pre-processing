e09160144d006eb748c6d8b5c6af9075b1983d0f
strikel <strikel@users.noreply.github.com>
2022-05-23 11:48:00 +1000

    Tweaks to pipeline

---
+++ func/task/emotional_conflict/create_fsf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# LT Strike
+# Code for the Emotional Conflict task in the QTAB dataset
+# This analysis pipeline is a work-in-progress - there are likely errors, bugs etc.
+
+code_dir=/home/user/Desktop/neurodesktop-storage/github/func/task/emotional_conflict
+
+# Create a .fsf file for each participant, based on a template
+# Find & replace QTABID in the template with the participant_id
+while read participant_id; do
+    cd "$code_dir" ||
+    echo "$participant_id"
+    cat "$code_dir"/template_full_analysis.fsf | sed s/QTABID/"$participant_id"/g > "$code_dir"/"$participant_id"_full_analysis.fsf
+    done < "$code_dir"/participant_id.txt
+
+# Run preprocessing and statistics
+# Local (Neurodesktop)
+# ml fsl/6.0.5.1 # needs to match the fsl directory given in template_preprocessing.fsf
+
+# feat "$participant_id".fsf
+# Note: Pay special attention to the registration between the field map and the structural scan
+# A modification to the epi_reg script may be needed if registration fails
+# Specifically these lines: 
+
+# Orig
+# register fmap to structural image
+# $FSLDIR/bin/flirt -in ${fmapmagbrain} -ref ${vrefbrain} -dof 6 -omat ${vout}_fieldmap2str_init.mat
+# $FSLDIR/bin/flirt -in ${fmapmaghead} -ref ${vrefhead} -dof 6 -init ${vout}_fieldmap2str_init.mat -omat ${vout}_fieldmap2str.mat -out ${vout}_fieldmap2str -nosearch
+
+# Modified (two lines of code above replaced with)
+# register fmap to structural image
+# $FSLDIR/bin/flirt -in ${fmapmagbrain} -ref ${vrefbrain} -dof 6 -omat ${vout}_fieldmap2str.mat -out ${vout}_fieldmap2str
+
+# see https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=FSL;360f54df.1910
+
