49227d1785abae00569c22ed86a79163d0298ca8
Lachlan Strike <strikel@users.noreply.github.com>
2022-06-24 16:11:18 +1000

    Update fMRIPrep_pipeline_partly_cloudy.sh

--- func/fMRIPrep_pipeline_partly_cloudy.sh
+++ func/fMRIPrep_pipeline_partly_cloudy.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 # LT Strike
 # Requires fMRIPrep & FreeSurfer
-# Requires T1w (skull-stripped MP2RAGE uniform image renamed as T1w), bold, field maps (optional - not currently working)
+# Requires T1w (skull-stripped MP2RAGE uniform image renamed as T1w), bold, field maps
 
 if [[ $# -eq 0 ]] ; then
     echo 'Please provide a participant_id'
     exit 0
 fi
 
-participantID="$*"
-ses="ses-02"
-echo Now running "$participantID"
-
-## Local (Neurodesk https://www.neurodesk.org/)
+# Local Run (Neurodesk https://www.neurodesk.org/)
 # Lauch through GUI: /Neurodesk/Functional Imaging/fmriprep or terminal: ml fmriprep/21.0.1
 # Make sure to include a link to the FreeSurfer licence file in the fMRIPrep call
-ml fmriprep/21.0.1
-data_dir=/neurodesktop-storage/qtab_bids
-t1w_dir=/neurodesktop-storage/qtab_bids/derivatives/MP2RAGE_preprocessing
-bids_dir=/neurodesktop-storage/qtab_analysis/fMRIPrep/bids
-output_dir=/neurodesktop-storage/qtab_analysis/fMRIPrep/output
-license_dir=/neurodesktop-storage/GitHub/pre-processing/anat
+ml fmriprep/21.0.1 
+
+participantID="$*"
+ses="ses-02"
 mem_mb="8000"
 num_threads="2"
+bids_dir=/neurodesktop-storage/qtab_bids
+t1w_dir=/neurodesktop-storage/qtab_bids/derivatives/MP2RAGE_preprocessing
+output_dir=/neurodesktop-storage/qtab_analysis/fMRIPrep
+license_dir=/neurodesktop-storage/GitHub/pre-processing/anat
+echo Now running "$participantID"
 
-
-## Local BIDS directory setup (initial run only)
-# mkdir -p "$bids_dir"
-# mkdir -p "$output_dir"
-# cp "$data_dir"/dataset_description.json "$bids_dir"
-# cp "$data_dir"/README "$bids_dir"
-# cp "$data_dir"/participants* "$bids_dir"
-
-# Organise the data (bold, t1w, field maps)
-mkdir -p "$bids_dir"/"$participantID"/"$ses"/func/
-mkdir -p "$bids_dir"/"$participantID"/"$ses"/anat/
-cp "$data_dir"/"$participantID"/"$ses"/func/"$participantID"_"$ses"_task-partlycloudy* "$bids_dir"/"$participantID"/"$ses"/func/
-cp "$data_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_UNIT1.json "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.json
+# Rename the skull-stripped MP2RAGE UNIT1 scan as T1w
 cp "$t1w_dir"/"$participantID"/"$participantID"_"$ses"_UNIT1_brain.nii.gz "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.nii.gz
-cp -r "$data_dir"/"$participantID"/"$ses"/fmap "$bids_dir"/"$participantID"/"$ses"/
+cp "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_inv-2_MP2RAGE.json "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.json
 
 # Run fMRIPrep
 fmriprep "$bids_dir"/ "$output_dir"/ participant --participant_label "$participantID" \
 	--skull-strip-t1w skip --fs-license-file "$license_dir"/.license -w /tmp/ \
 	--mem "$mem_mb" --nprocs "$num_threads" \
-	--ignore fieldmaps
+	-t partlycloudy --skip_bids_validation
 
 echo "$participantID" is complete
