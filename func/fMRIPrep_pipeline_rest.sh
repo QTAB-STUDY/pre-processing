b2a5f9bdfd3e99e7981e67593a5d20e29e31628e
strikel <lachlan.strike@gmail.com>
2022-09-07 09:22:23 +1000

    branch test

--- func/fMRIPrep_pipeline_rest.sh
+++ func/fMRIPrep_pipeline_rest.sh
@@ -13,6 +13,33 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+# Neurodesk Singularity Containers
+# https://www.neurodesk.org/docs/neurocontainers/singularity
+# fmriprep_21.0.1_20220329
+
+participantID="$*"
+ses="ses-01"
+bids_dir=/neurodesktop-storage/qtab_bids
+rest_dir=/neurodesktop-storage/qtab_bids/derivatives/rest
+t1w_dir=/neurodesktop-storage/qtab_bids/derivatives/MP2RAGE_preprocessing
+output_dir=/neurodesktop-storage/qtab_analysis/fMRIPrep
+license_dir=/neurodesktop-storage/GitHub/pre-processing/anat
+mem_mb="8000"
+num_threads="4"
+echo Now running "$participantID"
+
+# Run fMRIPrep
+fmriprep "$bids_dir"/ "$output_dir"/ participant --participant_label "$participantID" \
+        --fs-license-file "$license_dir"/.license -w /tmp/ \
+        --mem "$mem_mb" --nprocs "$num_threads" -t rest
+
+echo "$participantID" is complete
+
+
+
+
+
+
 ## Local (Neurodesk https://www.neurodesk.org/)
 # Lauch through GUI: /Neurodesk/Functional Imaging/fmriprep or terminal: ml fmriprep/21.0.1
 # Make sure to include a link to the FreeSurfer licence file in the fMRIPrep call
