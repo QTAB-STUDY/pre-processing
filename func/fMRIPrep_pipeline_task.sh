f8439beab727f517f9c413dfd502d9c5ac1673aa
strikel <lachlan.strike@gmail.com>
2022-09-08 14:49:46 +1000

    Adapt for singularity

--- func/fMRIPrep_pipeline_task.sh
+++ func/fMRIPrep_pipeline_task.sh
@@ -10,33 +10,37 @@
 # Susceptibility Distortion Correction (SDC) - field maps or field map less estimation (--use-syn-sdc)
 # Field maps require metadata field in field map json: "IntendedFor":["ses-02/func/participant_id_ses-02_task-partlycloudy_bold.nii.gz"]
 
+module load singularity/3.7.1
+
 if [[ $# -eq 0 ]] ; then
     echo 'Please provide a participant_id'
     exit 0
 fi
 
-# Local (Neurodesk https://www.neurodesk.org/)
-# Lauch through GUI: /Neurodesk/Functional Imaging/fmriprep or terminal: ml fmriprep/21.0.1
-# Make sure to include a link to the FreeSurfer licence file in the fMRIPrep call
-ml fmriprep/21.0.1 
+# Neurodesk Singularity Containers
+# https://www.neurodesk.org/docs/neurocontainers/singularity
+# fmriprep_21.0.1_20220329
 
 participantID="$*"
 ses="ses-02"
-mem_mb="12000"
-num_threads="2"
-bids_dir=/neurodesktop-storage/qtab_bids
-t1w_dir=/neurodesktop-storage/qtab_bids/derivatives/MP2RAGE_preprocessing
-output_dir=/neurodesktop-storage/qtab_analysis/fMRIPrep
-license_dir=/neurodesktop-storage/GitHub/pre-processing/anat
+container_dir=/working/lab_sarahme/lachlanS/containers
+license_dir=/mnt/backedup/home/lachlanS/bin/qtab/sMRI/FreeSurfer
+mem_mb="16000"
+num_threads="4"
+
 echo Now running "$participantID"
 
-# Rename the skull-stripped MP2RAGE UNIT1 scan as T1w
-cp "$t1w_dir"/"$participantID"/"$participantID"_"$ses"_UNIT1_brain.nii.gz "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.nii.gz
-cp "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_inv-2_MP2RAGE.json "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.json
+# Rename the masked UNIT1 scan as T1w
+cp "$bids_dir"/derivatives/freesurfer/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_UNIT1-masked.nii.gz "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.nii.gz
+cp "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_UNIT1.json "$bids_dir"/"$participantID"/"$ses"/anat/"$participantID"_"$ses"_T1w.json
 
-# Run fMRIPrep
-fmriprep "$bids_dir"/ "$output_dir"/ participant --participant_label "$participantID" \
-	--skull-strip-t1w skip --fs-license-file "$license_dir"/.license -w /tmp/ \
+singularity run --cleanenv \
+    --bind /working/lab_sarahme/lachlanS/qtab_data_task:/data \
+    --bind /working/lab_sarahme/lachlanS/qtab_analysis/fMRIPrep:/output \
+    "$container_dir"/freesurfer_7.3.2_20220812.simg \
+    /data/ /output/ \
+    participant --participant_label "$participantID" \
+	--fs-license-file "$license_dir"/FreeSurfer_license \
 	--mem "$mem_mb" --nprocs "$num_threads" \
 	-t partlycloudy --skip_bids_validation 
 #	--output-spaces MNIPediatricAsym:res-native:cohort-5 \
